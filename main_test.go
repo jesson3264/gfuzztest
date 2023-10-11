2766d68430d57b70935edaeaaa9929f02d1e60fd
jesson-deng <jesson3264@163.com>
2023-10-11 22:11:14 +0800

    add

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestDemo(t *testing.T) {
+	ch := make(chan int)
+	go func() {
+		ch <- 1
+	}()
+
+	select {
+	case <-ch:
+		fmt.Println("Normal")
+	case <-time.After(300 * time.Millisecond):
+		fmt.Println("Should be buggy")
+	}
+}
